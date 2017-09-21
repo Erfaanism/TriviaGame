c9ebe9273acb4f7664504441d5a45d514754fe1c
Ali Darvishi <erfaanism@gmail.com>
2017-09-21 02:30:30 -0700

    the initial design has been implemented

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+var intQuestionNum = 0;
+var intCorrect = 0;
+var intIncorrect = 0;
+var objQuestion = null;
+var strAnswer = "";
+var strWrong1 = "";
+var strWrong2 = "";
+var strWrong3 = "";
+
+
+
+
+
+
+	$("#start").on("click", function() {
+		$("#containerIntro").css("display", "none");
+		$("#containerQuestions").css("display", "block");
+
+	});
+
+})
